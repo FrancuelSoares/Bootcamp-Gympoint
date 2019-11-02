96eef32129c87025c60ca20bf9a7982cf47f87ff
Françuel Soares <fs.francuelsoares@gmail.com>
2019-11-01 21:19:50 -0300

    0.7.0
    # New - Added connection to User Model
    
    # Fix - User Model

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,17 @@
 import { Router } from 'express';
 
+import User from './app/models/User';
+
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  res.json({ ok: 'Ola mundo!' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Françuel',
+    email: 'fran@gmail.com',
+    password_hash: 'behvucyvewch'
+  });
+
+  res.json(user);
 });
 
 export default routes;
